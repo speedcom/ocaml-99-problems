83b869a0873191d201ae7abc4b1494a5ab323ddc
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-12-17 23:28:26 +0100

    solved 3 problem

--- solution.ml
+++ solution.ml
@@ -13,3 +13,17 @@ let rec last_two list =
     | el1 :: el2 :: [] -> Some (el1, el2)
     | hd :: tail       -> last_two tail
   ;;
+
+(* Problem 3, first version *)
+let rec at idx list =
+    match list with
+    | []                       -> None
+    | el :: _  when idx == 1   -> Some el
+    | _ :: tail                -> at (idx-1) tail
+  ;;
+
+(* Problem 3, second version *)
+let rec at idx = function
+    | []     -> None
+    | h :: t -> if idx = 1 then Some h else at (idx-1) t
+  ;;
