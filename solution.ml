4f1a909f31dc698c382cfccff66d628d8540c557
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-12-17 23:28:14 +0100

    solved 2 problem

--- solution.ml
+++ solution.ml
@@ -5,3 +5,11 @@ let rec last list =
     | [el]      -> Some el
     | _ :: tail -> last tail
   ;;
+
+(* Problem 2 *)
+let rec last_two list =
+    match list with
+    | []               -> None
+    | el1 :: el2 :: [] -> Some (el1, el2)
+    | hd :: tail       -> last_two tail
+  ;;
